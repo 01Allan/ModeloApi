e0860e270a445bd421accfe4572ffe3ace0cd3c3
AllanPineda <allanepinedag@gmail.com>
2024-12-07 04:03:30 -0600

    Para desplegar xd

--- proyectoFinal/app/models/loader.py
+++ proyectoFinal/app/models/loader.py
@@ -1,7 +1,9 @@
 import joblib
+import os
 
 def load_model(model_path: str):
+    absolute_path = os.path.join(os.path.dirname(__file__), "..", model_path)
     try:
-        return joblib.load(model_path)
+        return joblib.load(absolute_path)
     except FileNotFoundError:
         raise RuntimeError(f"No se encontró el modelo en la ruta: {model_path}")
