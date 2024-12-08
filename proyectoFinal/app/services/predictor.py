510c572415b5d4a77ba93a306e70b220bf4b57d2
AllanPineda <allanepinedag@gmail.com>
2024-12-07 19:44:53 -0600

    Pruebas 07122024_2_correcciones0

--- proyectoFinal/app/services/predictor.py
+++ proyectoFinal/app/services/predictor.py
@@ -15,19 +15,25 @@ def make_prediction(model, input_data):
         if missing_fields:
             raise ValueError(f"Faltan campos requeridos en input_data: {missing_fields}")
 
-        input_features = pd.DataFrame([[
-            input_data["Age"],
-            input_data["Gender"],
-            input_data["Support_Calls"],
-            input_data["Payment_Delay"],
-            input_data["Subscription_Type"],
-            input_data["Total_Spend"],
-            input_data["Last_Interaction"]
-        ]], columns=[
-            "Age", "Gender", "Support_Calls", 
-            "Payment_Delay", "Subscription_Type", 
-            "Total_Spend", "Last_Interaction"
-        ])
+        column_mapping = {
+            "Age": "Age",
+            "Gender": "Gender",
+            "Support_Calls": "Support Calls",
+            "Payment_Delay": "Payment Delay",
+            "Subscription_Type": "Subscription Type",
+            "Total_Spend": "Total Spend",
+            "Last_Interaction": "Last Interaction"
+        }
+
+        input_features = pd.DataFrame([{
+            column_mapping["Age"]: input_data["Age"],
+            column_mapping["Gender"]: input_data["Gender"],
+            column_mapping["Support_Calls"]: input_data["Support_Calls"],
+            column_mapping["Payment_Delay"]: input_data["Payment_Delay"],
+            column_mapping["Subscription_Type"]: input_data["Subscription_Type"],
+            column_mapping["Total_Spend"]: input_data["Total_Spend"],
+            column_mapping["Last_Interaction"]: input_data["Last_Interaction"]
+        }])
 
         print("Características de entrada para el modelo:", input_features)
 
@@ -42,5 +48,6 @@ def make_prediction(model, input_data):
             "probability": probability,
             "interpretation": "Cliente en riesgo de desertar" if prediction == 1 else "Cliente leal"
         }
+    
     except Exception as e:
         raise RuntimeError(f"Error durante la predicción: {str(e)}")
