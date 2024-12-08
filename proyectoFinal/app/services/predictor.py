f83ba6ccd60dbf36e1fde9952d50e90110f1419a
AllanPineda <allanepinedag@gmail.com>
2024-12-07 19:12:46 -0600

    Pruebas 07122024_2_correcciones

--- proyectoFinal/app/services/predictor.py
+++ proyectoFinal/app/services/predictor.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def make_prediction(model, input_data):
     try:
@@ -11,15 +12,15 @@ def make_prediction(model, input_data):
         if missing_fields:
             raise ValueError(f"Faltan campos requeridos en input_data: {missing_fields}")
 
-        input_features = np.array([[
-            input_data.Age, 
-            input_data.Gender, 
-            input_data.Support_Calls,
-            input_data.Payment_Delay, 
-            input_data.Subscription_Type,
-            input_data.Total_Spend, 
-            input_data.Last_Interaction
-        ]])
+        input_features = pd.DataFrame([{
+            "Age": input_data.Age,
+            "Gender": input_data.Gender,
+            "Support_Calls": input_data.Support_Calls,
+            "Payment_Delay": input_data.Payment_Delay,
+            "Subscription_Type": input_data.Subscription_Type,
+            "Total_Spend": input_data.Total_Spend,
+            "Last_Interaction": input_data.Last_Interaction
+        }])
 
         prediction = model.predict(input_features)[0]
         probability = model.predict_proba(input_features)[0].tolist()
