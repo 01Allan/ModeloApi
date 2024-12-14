5d18ce5c96362f06976c994d4f9217fa7319c949
Allan Pineda <allanepinedag@gmail.com>
2024-12-14 01:31:32 -0600

    Integracion con la base de datos_ Correcciones02

--- proyectoFinal/app/services/predictor.py
+++ proyectoFinal/app/services/predictor.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from app.services.database import save_prediction_to_db
+from app.services.logger import logger
 
 def normalize_data(data):
     """
@@ -14,6 +15,7 @@ def normalize_data(data):
 
 def make_prediction(model, input_data):
     try:
+        
         if not isinstance(input_data, dict):
             input_data = input_data.dict()
 
@@ -22,22 +24,36 @@ def make_prediction(model, input_data):
             "Support_Calls", "Payment_Delay", 
             "Total_Spend", "Last_Interaction"
         ]
+
         numeric_data = {key: input_data[key] for key in numeric_features}
         numeric_df = pd.DataFrame([numeric_data])
 
+        column_mapping = {
+            "Last_Interaction": "Last Interaction",
+            "Payment_Delay": "Payment Delay",
+            "Support_Calls": "Support Calls",
+            "Total_Spend": "Total Spend",
+            "Usage_Frequency": "Usage Frequency",
+            "Contract_Length": "Contract Length",
+            "Gender": "Gender",
+            "Subscription_Type": "Subscription Type"
+        }
+        numeric_df.rename(columns=column_mapping, inplace=True)
+
         normalized_data = normalize_data(numeric_df)
-        normalized_df = pd.DataFrame(normalized_data, columns=numeric_features)
+        normalized_df = pd.DataFrame(normalized_data, columns=numeric_df.columns)
 
         prediction = model.predict(normalized_df)[0]
         probability = model.predict_proba(normalized_df)[0].tolist()
 
         db_data = {
             **input_data, 
-            "Churn": int(prediction), 
-            "Probability": probability[prediction] 
+            "Churn": int(prediction),  
+            "Probability": probability[prediction]  
         }
 
         save_prediction_to_db(db_data)
+        logger.info(f"Predicción realizada con éxito para CustomerID {input_data.get('CustomerID')}")
 
         return {
             "prediction": int(prediction),
@@ -46,4 +62,5 @@ def make_prediction(model, input_data):
         }
 
     except Exception as e:
+        logger.error(f"Error durante la predicción para CustomerID {input_data.get('CustomerID', 'desconocido')}: {str(e)}")
         raise RuntimeError(f"Error durante la predicción: {str(e)}")
