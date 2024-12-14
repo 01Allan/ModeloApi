9c7845df7773d40c403da037e3ed5e645dbe6040
Allan Pineda <allanepinedag@gmail.com>
2024-12-14 04:53:35 -0600

    coors

--- proyectoFinal/app/main.py
+++ proyectoFinal/app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import predictions, health, stats
 
 app = FastAPI(
     title="API de Predicción de Deserción",
     description="Esta API predice la deserción de clientes y proporciona estadísticas del modelo.",
     version="1.0.0",
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite solicitudes desde cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los headers
 )
 
 @app.get("/")
